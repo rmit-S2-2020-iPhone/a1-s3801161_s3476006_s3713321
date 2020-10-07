c8780b6057fe3228f501d6022caba7587bcc0bf8
duanxinhuan <duanxinhuan@163.com>
2020-10-08 00:28:59 +1100

    cxvfb fgdsvdafv

--- CountDone/AppDelegate.swift
+++ CountDone/AppDelegate.swift
@@ -10,7 +10,7 @@ import UIKit
 import CoreData
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+xclass AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
     var coordinator: AppCoordinator?
