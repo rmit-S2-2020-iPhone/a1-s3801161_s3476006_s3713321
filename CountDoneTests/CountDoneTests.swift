7f4a45efd80f8f3550b5a3189353c6a9c89ce013
duanxinhuan <duanxinhuan@163.com>
2020-10-11 14:51:30 +1100

    change

--- CountDoneTests/CountDoneTests.swift
+++ CountDoneTests/CountDoneTests.swift
@@ -20,42 +20,7 @@ class CountDoneTests: XCTestCase {
     }
 
     //positive tests for the fetch user function
-    func testApiFectchPositive() {
-        let rest = REST_Request()
-        rest.emptyUsers()
-        rest.getUsers(withEmail: "duanxinhuan@163.com")
-        sleep(1)
-        let users =  rest.getAccounts()
-        XCTAssertEqual(users.count, 3, "there are 3 users intotal")
-        XCTAssertTrue(rest.filterId())
-    }
-    
-    //negative test for the fetch user function
-    func testApiFetchNegative(){
-        let rest = REST_Request()
-        rest.emptyUsers()
-        let users =  rest.getAccounts()
-        XCTAssertEqual(users.count, 0, "there are 3 users intotal")
-        XCTAssertFalse(rest.filterId())
-        rest.getUsers(withEmail: "duanxinhuan@163.com")
-        
-    }
-    
-    func testApiFetchEventPositive(){
-        let rest = REST_Request()
-        rest.emptyEvents()
-        rest.getEvents(withId: 1)
-        sleep(1)
-        let events = try! rest.context.fetch(Task.fetchRequest())
-        XCTAssertEqual(events.count,6,"good fetch")
-    }
-    
-    func testApiFetchEventNegative(){
-        let rest = REST_Request()
-        rest.emptyEvents()
-        let events = try! rest.context.fetch(Task.fetchRequest())
-        XCTAssertEqual(events.count,0,"good fetch")
-    }
+
     
     func testExample() {
         // This is an example of a functional test case.
